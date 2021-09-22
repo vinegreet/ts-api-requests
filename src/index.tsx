3cb82cc7acf0313e27d065ca4a712049d7fd6591
Roman Zots <vinegreet@gmail.com>
2021-09-22 17:34:48 +0300

    Add DataState to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import DataState from './context/DataState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataState>
+      <App />
+    </DataState>
   </React.StrictMode>,
   document.getElementById('root')
 );
