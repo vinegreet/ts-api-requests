58220e7d2c3d6dd8e3d5524dd01ca381774bc372
Roman Zots <vinegreet@gmail.com>
2021-09-23 14:48:25 +0300

    Add ChoseTheFilm component

---
+++ src/components/ChooseTheFilm/ChooseTheFilm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './chooseTheFilm.css';
+
+const ChooseTheFilm: React.FC = () => {
+  return (
+    <div className="choose-the-film">
+      <div className="choose-the-film_title">&larr; Please choose the film</div>
+    </div>
+  );
+}
+
+export default ChooseTheFilm;
