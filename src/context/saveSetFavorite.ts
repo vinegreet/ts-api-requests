d165fcafb4a0a6e48221a937da4030857699a5ed
Roman Zots <vinegreet@gmail.com>
2021-09-23 14:45:41 +0300

    Extract wrapper function for setting local storage

---
+++ src/context/saveSetFavorite.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from "react";
+import { IAction } from "./dataReducer";
+import { saveFavorites } from './localStorage';
+
+export const saveSetFavorite = (
+  dispatch: Dispatch<IAction>,
+  favorites: number[],
+  id: number,
+  actionType: 'SET_FAVORITE' | 'UNSET_FAVORITE',
+): void => {
+  saveFavorites(favorites)
+    .then(() => {
+      dispatch({
+        type: actionType,// TODO: create actions types constants
+        payload: id,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      // If the local storage update fails, the favorite should not be added.
+      // TODO: looks like user's privacy mode does not allow the use of local storage. Show them a message about it.
+    });
+};
